b0afd20544dca00dee742b5fc522b51a3fc9d9ed
Navneet Singh <navneetsingh1825@gmail.com>
2025-07-05 22:11:23 +0530

    Remove CORS configuration from server setup

--- server/server.js
+++ server/server.js
@@ -1,16 +1,9 @@
 import { app } from "./app.js";
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
-import cors from "cors"; 
 
 dotenv.config();
 
-// Configure CORS to allow frontend requests
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
